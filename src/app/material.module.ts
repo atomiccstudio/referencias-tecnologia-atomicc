733488f434e4162f31e79233ed62ae88acb52a75
Elaine Manoelle <elaine.manoelle@pmipe.org.br>
2020-10-18 22:27:58 -0300

    Developin the feature of filter links

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   exports: [
     MatTableModule,
+    MatInputModule
   ]
 })
 export class MaterialModule {}
