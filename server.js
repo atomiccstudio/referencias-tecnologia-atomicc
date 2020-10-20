580dfc513e1721ffb8a4ca1b778a45c3ce07f68e
Tathiane Pinto <tathiane.claudia@gmail.com>
2020-10-20 00:09:23 -0300

    Adjusting server path file

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + 'dist/referencias-tecnologia-atomicc'));
+app.use(express.static(__dirname + '/dist/referencias-tecnologia-atomicc'));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(__dirname + 'dist/referencias-tecnologia-atomicc/index.html'));
+res.sendFile(path.join(__dirname + '/dist/referencias-tecnologia-atomicc/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
