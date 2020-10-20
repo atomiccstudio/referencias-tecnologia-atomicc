b7d71470d8893dddb05d667d19704db592c2b5cb
Tathiane Pinto <tathiane.claudia@gmail.com>
2020-10-19 23:22:23 -0300

    Adjusting server file for the right folder

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + 'dist/atomicc-hacktoberfest'));
+app.use(express.static(__dirname + 'dist/referencias-tecnologia-atomicc'));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(__dirname + '/dist/atomicc-hacktoberfest/index.html'));
+res.sendFile(path.join(__dirname + '/dist/referencias-tecnologia-atomicc/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
