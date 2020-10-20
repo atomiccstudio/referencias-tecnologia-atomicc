284db97c5bc4919a0c678bb7933cf7578e4d5c4a
Tathiane Pinto <tathiane.claudia@gmail.com>
2020-10-20 00:39:01 -0300

    New server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+//Importa as dependências que acabamos de instalar
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
 app.use(express.static(__dirname + '/dist/referencias-tecnologia-atomicc'));
 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(__dirname + '/dist/referencias-tecnologia-atomicc/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/referencias-tecnologia-atomicc/index.html'));
 });
 
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
