a701e825b82265b71670410161bc63fb1cbb59cd
camilars <camila.rs122@gmail.com>
2020-10-20 00:31:09 -0300

    Inclusão de um objeto no team.js

--- src/app/datasource/team.ts
+++ src/app/datasource/team.ts
@@ -22,6 +22,29 @@ export default {
             "urlReadMore": "https://www.linkedin.com/in/elainemanoelle",
             "bio": "Inquieta, audaciosa, e obstinada, Elaine Manoelle está na área de tecnologia há 7 anos. Apaixonada pela área, muito mais que salário, o que a move é o propósito e a consciência de que ser uma mulher numa área majoritariamente ocupada por homens, sobretudo sendo oriunda de escola pública, lhe dá a possibilidade - que encara como missão - de inspirar outras e outros que vem do mesmo lugar. Trabalha há quase 1 ano e meio na Accenture como Analista de Sistemas Senior, desenvolvendo sistemas web para outras grandes empresas - que terão como usuários finais centenas ou milhares de pessoas. Suas principais hard skills são Javascript (no frontend com os frameworks Angular e Vue - no backend com NodeJS (express), Html5, CSS3, Git, Wordpress e Scrum. Além de PHP (com framework Laravel), linguagem com a qual trabalhou boa parte da sua carreira. Possui certificação CAPM pelo PMI e CSM pela Scrum Alliance. Estou estudando React & React Native, testes unitários e de integração, e design patterns. Além do emprego formal, também atua como freelancer com WordPress no desenvolvimento de lojas virtuais e sites institucionais. Procurando se desafiar constantemente e nunca me permitir estar na zona de conforto.",
             
+        },
+        {
+            "key": "camila",
+            "name": "Camila Silva",
+            "city": "Paulista",
+            "state": "Pernambuco",
+            "photo": "./assets/img/team/camila.jpg",
+            "role": "Web Developer",
+            "company": "Accenture",
+            "office": "Atomicc Studio",
+            "socialNetworks": [
+                {
+                    "icon": "fa fa-linkedin",
+                    "url": "https://www.linkedin.com/in/camilarrsilva",
+                },
+                {
+                    "icon": "fa fa-github",
+                    "url": "https://github.com/camilars"
+                }
+            ],
+            "urlReadMore": "https://www.linkedin.com/in/camilarrsilva",
+            "bio": "Mãe de duas filhas, Analista de Sistemas Pleno na Accenture há mais de 1 ano, estudante bolsista do curso de Análise e Desenvolvimento de Sistemas, entusiasta por tecnologia e que anseia por conhecimento. Encontrou na TI não apenas o sustento da família bem como o prazer de trabalhar com o que ama. Tem compreenção do quanto é importante a inclusão feminina no mercado de Tecnologia e das dificulades encontradas nesse caminho. Tem como propósito inspirar e ajudar outras mulheres na construção de suas carreiras na tecnologia, e como dever retribuir o apoio, contribuição e conhecimento que recebeu das cominudades voltadas para a capacitação feminina na tecnologia.",
+            
         }
     ]
 }
