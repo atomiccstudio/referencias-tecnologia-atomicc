41250f00b7b4e1ef8906b7e3dd26f63f51d7ecda
Elaine Manoelle <elaine.manoelle@pmipe.org.br>
2020-10-31 18:13:16 -0300

    Adjustments on project

--- src/app/datasource/references.ts
+++ src/app/datasource/references.ts
@@ -1,265 +1,182 @@
 export default {
     links: [
         {
-            "title": "Cloud AWS: como funciona a computação em nuvem da Amazon",
-            "type": "Artigo",
-            "url": "https://teltecsolutions.com.br/mundo/cloud-aws",
-            "keywords": [
-                "cloud",
-                "aws"
-            ]
+            title: 'Cloud AWS: como funciona a computação em nuvem da Amazon',
+            type: 'Artigo',
+            url: 'https://teltecsolutions.com.br/mundo/cloud-aws',
+            keywords: ['cloud', 'aws'],
         },
         {
-            "title": "O que é a computação em nuvem?",
-            "type": "Artigo",
-            "url": "https://aws.amazon.com/pt/what-is-cloud-computing/",
-            "keywords": [
-                "cloud",
-                "aws"
-            ]
+            title: 'O que é a computação em nuvem?',
+            type: 'Artigo',
+            url: 'https://aws.amazon.com/pt/what-is-cloud-computing/',
+            keywords: ['cloud', 'aws'],
         },
         {
-            "title": "Angular: Redux e NgRx",
-            "type": "Artigo",
-            "url": "https://balta.io/artigos/angular-redux-ngrx",
-            "keywords": [
-                "angular",
-                "ngrx"
-            ]
+            title: 'Angular: Redux e NgRx',
+            type: 'Artigo',
+            url: 'https://balta.io/artigos/angular-redux-ngrx',
+            keywords: ['angular', 'ngrx'],
         },
         {
-            "title": "Construindo aplicações front-end reativas com NgRx",
-            "type": "Artigo",
-            "url": "https://movile.blog/construindo-aplicacoes-front-end-reativas-com-ngrx/",
-            "keywords": [
-                "angular",
-                "ngrx"
-            ]
+            title: 'Construindo aplicações front-end reativas com NgRx',
+            type: 'Artigo',
+            url:
+                'https://movile.blog/construindo-aplicacoes-front-end-reativas-com-ngrx/',
+            keywords: ['angular', 'ngrx'],
         },
         {
-            "title": "Documentação oficial Kafka",
-            "type": "Artigo",
-            "url": "https://kafka.apache.org/intro",
-            "keywords": [
-                "kafka",
-                "documentação"
-            ]
+            title: 'Documentação oficial Kafka',
+            type: 'Artigo',
+            url: 'https://kafka.apache.org/intro',
+            keywords: ['kafka', 'documentação'],
         },
         {
-            "title": "Kafka na prática",
-            "type": "Artigo",
-            "url": "https://medium.com/trainingcenter/apache-kafka-codifica%C3%A7%C3%A3o-na-pratica-9c6a4142a08f",
-            "keywords": [
-                "kafka",
-                "prática"
-            ]
+            title: 'Kafka na prática',
+            type: 'Artigo',
+            url:
+                'https://medium.com/trainingcenter/apache-kafka-codifica%C3%A7%C3%A3o-na-pratica-9c6a4142a08f',
+            keywords: ['kafka', 'prática'],
         },
         {
-            "title": "O que é esse tal de Apache Kafka?",
-            "type": "Artigo",
-            "url": "https://medium.com/@gabrielqueiroz/o-que-%C3%A9-esse-tal-de-apache-kafka-a8f447cac028",
-            "keywords": [
-                "kafka",
-                "Apache"
-            ]
+            title: 'O que é esse tal de Apache Kafka?',
+            type: 'Artigo',
+            url:
+                'https://medium.com/@gabrielqueiroz/o-que-%C3%A9-esse-tal-de-apache-kafka-a8f447cac028',
+            keywords: ['kafka', 'Apache'],
         },
         {
-            "title": "React Router v4",
-            "type": "Artigo",
-            "url": "https://medium.com/collabcode/roteamento-no-react-com-os-poderes-do-react-router-v4-fbc191b9937d",
-            "keywords": [
-                "React",
-                "Router"
-            ]
+            title: 'React Router v4',
+            type: 'Artigo',
+            url:
+                'https://medium.com/collabcode/roteamento-no-react-com-os-poderes-do-react-router-v4-fbc191b9937d',
+            keywords: ['React', 'Router'],
         },
         {
-            "title": "Padrões em React: Provider Pattern",
-            "type": "Artigo",
-            "url": "https://oieduardorabelo.medium.com/padr%C3%B5es-em-react-provider-pattern-b520c37ed733",
-            "keywords": [
-                "React",
-                "Pattern"
-            ]
+            title: 'Padrões em React: Provider Pattern',
+            type: 'Artigo',
+            url:
+                'https://oieduardorabelo.medium.com/padr%C3%B5es-em-react-provider-pattern-b520c37ed733',
+            keywords: ['React', 'Pattern'],
         },
         {
-        "title": "Como funciona o JavaScript",
-        "type": "Artigo",
-        "url": "https://blog.sessionstack.com/tagged/tutorial",
-        "keywords": [
-            "JavaScript"
-        ]
-    },
-        {
-         "title": "Como funciona o JavaScript",
-            "type": "Artigo",
-            "url": "https://blog.sessionstack.com/tagged/tutorial",
-            "keywords": [
-                 "JavaScript"
-       ]
-         },
-{
-    "title": "Artigos sobre Angular e React",
-        "type": "Artigo",
-            "url": "https://indepth.dev/",
-                "keywords": [
-                    "Angular",
-                    "React"
-                ]
-},
-{
-    "title": "Curso de Angular",
-        "type": "Vídeo",
-            "url": "https://www.youtube.com/watch?v=tPOMG0D57S0&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G&index=1",
-                "keywords": [
-                    "Angular",
-                    "Curso"
-                ]
-},
-{
-    "title": "Curso de Git e GitHub",
-        "type": "Vídeo",
-            "url": "https://www.youtube.com/playlist?list=PLHz_AreHm4dm7ZULPAmadvNhH6vk9oNZA",
-                "keywords": [
-                    "Git",
-                    "GitHub",
-                    "Curso"
-                ]
-},
-{
-    "title": "Empacado no seu código? Conheça o Stack Overflow",
-        "type": "Site",
-            "url": "https://pt.stackoverflow.com/",
-                "keywords": [
-                    "Stack",
-                    "Programação",
-                    "Stackoverflow"
-                ]
-},
-{
-    "title": "O que é API, REST e RESTFULL?",
-        "type": "Vídeo",
-            "keywords": [
-                "Stack",
-                "Programação",
-                "Stackoverflow"
-            ]
+            title: 'Como funciona o JavaScript',
+            type: 'Artigo',
+            url: 'https://blog.sessionstack.com/tagged/tutorial',
+            keywords: ['JavaScript'],
         },
         {
-            "title": "Framework Django",
-            "type": "Site",
-            "url": "https://www.djangoproject.com/",
-            "keywords": [
-                "Django",
-                "Frameworks"
-            ]
+            title: 'Como funciona o JavaScript',
+            type: 'Artigo',
+            url: 'https://blog.sessionstack.com/tagged/tutorial',
+            keywords: ['JavaScript'],
         },
         {
-            "title": "Curso de JavaScript e ECMAScript para Iniciantes",
-            "type": "Video",
-            "url": "https://www.youtube.com/playlist?list=PLHz_AreHm4dlsK3Nr9GVvXCbpQyHQl1o1",
-            "keywords": [
-                "JavaScript",
-                "ECMAScript",
-                "Curso"
-            ]
+            title: 'Artigos sobre Angular e React',
+            type: 'Artigo',
+            url: 'https://indepth.dev/',
+            keywords: ['Angular', 'React'],
         },
         {
-            "title": "Programação para todos: The Huxley!",
-            "type": "Site",
-            "url": "https://thehuxley.com",
-            "keywords": [
-                "Huxley",
-                "Programação"
-            ]
+            title: 'Curso de Angular',
+            type: 'Vídeo',
+            url:
+                'https://www.youtube.com/watch?v=tPOMG0D57S0&list=PLGxZ4Rq3BOBoSRcKWEdQACbUCNWLczg2G&index=1',
+            keywords: ['Angular', 'Curso'],
         },
         {
-            "title": "Estude tecnologia com a Alura!",
-            "type": "Site",
-            "url": "https://www.alura.com.br",
-            "keywords": [
-                "Alura",
-                "Cursos"
-            ]
+            title: 'Curso de Git e GitHub',
+            type: 'Vídeo',
+            url:
+                'https://www.youtube.com/playlist?list=PLHz_AreHm4dm7ZULPAmadvNhH6vk9oNZA',
+            keywords: ['Git', 'GitHub', 'Curso'],
         },
         {
-            "title": "O que é API, REST e RESTFULL?",
-            "type": "Vídeo",
-            "url": "https://www.youtube.com/watch?v=ghTrp1x_1As&feature=youtu.be",
-                "keywords": [
-                    "api",
-                    "rest",
-                    "restfull"
-                ]
-},
-{
-    "title": "Como usar Git e Github na prática: Guia para iniciantes",
-        "type": "Vídeo",
-            "url": "https://www.youtube.com/watch?v=2alg7MQ6_sI",
-                "keywords": [
-                    "git",
-                    "github"
-                ]
-},
-{
-    "title": "Qual a diferença entre Inteligência Artificial, Machine Learning, Data Science, Deep Learning, etc?",
-        "type": "Vídeo",
-            "url": "https://www.youtube.com/watch?v=ccZ2pyr3YDw&t=1s",
-                "keywords": [
-                    "Inteligência Artificial",
-                    "IA",
-                    "machine learning",
-                    "data science",
-                    "deep learning",
-                ]
-},
-{
-    "title": "Gerenciando estado com NGRX",
-        "type": "Vídeo",
-            "keywords": [
-                "inteligencia artificial",
-                "IA",
-                "machine learning",
-                "data science",
-                "deep learning",
-            ]
+            title: 'Empacado no seu código? Conheça o Stack Overflow',
+            type: 'Site',
+            url: 'https://pt.stackoverflow.com/',
+            keywords: ['Stack', 'Programação', 'Stackoverflow'],
         },
         {
-            "title": "Gerenciando estado com NGRX",
-            "type": "Vídeo",
-            "url": "https://www.youtube.com/watch?v=BobNwRgz6Uw&t=685s",
-                "keywords": [
-                    "ngrx",
-                    "gerencia de estado"
-                ]
-},
-{
-"title": "Aprendendo programação funcional com JavaScript",
-    "type": "Vídeo",
-        "url": "https://www.youtube.com/watch?v=e-5obm1G_FY&list=PLFIlIa2DY4kzQlsLEwYX3ZrwtahgSLFur&index=4&t=6s",
-            "keywords": [
-                "JavaScript",
-                "Funcional"
-            ]
-},
-{
-    "title": "O que é Next.js?",
-    "type": "Artigo",
-    "url": "https://segredo.dev/o-que-e-next-js/",
-    "keywords": [
-        "next",
-        "frontend",
-        "backend"
-    ]
-},
-{
-    "title": "Introduction to GraphQL",
-    "type": "Artigo",
-    "url": "https://graphql.org/learn/",
-    "keywords": [
-        "GraphQL",
-        "Introduction",
-        "iniciante"
-    ]
-}
-    ]
+            title: 'O que é API, REST e RESTFULL?',
+            type: 'Vídeo',
+            url: 'https://www.youtube.com/watch?v=ghTrp1x_1As&t=3s',
+            keywords: ['Stack', 'Programação', 'Stackoverflow'],
+        },
+        {
+            title: 'Framework Django',
+            type: 'Site',
+            url: 'https://www.djangoproject.com/',
+            keywords: ['Django', 'Frameworks'],
+        },
+        {
+            title: 'Curso de JavaScript e ECMAScript para Iniciantes',
+            type: 'Video',
+            url:
+                'https://www.youtube.com/playlist?list=PLHz_AreHm4dlsK3Nr9GVvXCbpQyHQl1o1',
+            keywords: ['JavaScript', 'ECMAScript', 'Curso'],
+        },
+        {
+            title: 'Programação para todos: The Huxley!',
+            type: 'Site',
+            url: 'https://thehuxley.com',
+            keywords: ['Huxley', 'Programação'],
+        },
+        {
+            title: 'Estude tecnologia com a Alura!',
+            type: 'Site',
+            url: 'https://www.alura.com.br',
+            keywords: ['Alura', 'Cursos'],
+        },
+        {
+            title: 'O que é API, REST e RESTFULL?',
+            type: 'Vídeo',
+            url: 'https://www.youtube.com/watch?v=ghTrp1x_1As&feature=youtu.be',
+            keywords: ['api', 'rest', 'restfull'],
+        },
+        {
+            title: 'Como usar Git e Github na prática: Guia para iniciantes',
+            type: 'Vídeo',
+            url: 'https://www.youtube.com/watch?v=2alg7MQ6_sI',
+            keywords: ['git', 'github'],
+        },
+        {
+            title:
+                'Qual a diferença entre Inteligência Artificial, Machine Learning, Data Science, Deep Learning, etc?',
+            type: 'Vídeo',
+            url: 'https://www.youtube.com/watch?v=ccZ2pyr3YDw&t=1s',
+            keywords: [
+                'Inteligência Artificial',
+                'IA',
+                'machine learning',
+                'data science',
+                'deep learning',
+            ],
+        },
+        {
+            title: 'Gerenciando estado com NGRX',
+            type: 'Vídeo',
+            url: 'https://www.youtube.com/watch?v=BobNwRgz6Uw&t=685s',
+            keywords: ['ngrx', 'gerencia de estado'],
+        },
+        {
+            title: 'Aprendendo programação funcional com JavaScript',
+            type: 'Vídeo',
+            url:
+                'https://www.youtube.com/watch?v=e-5obm1G_FY&list=PLFIlIa2DY4kzQlsLEwYX3ZrwtahgSLFur&index=4&t=6s',
+            keywords: ['JavaScript', 'Funcional'],
+        },
+        {
+            title: 'O que é Next.js?',
+            type: 'Artigo',
+            url: 'https://segredo.dev/o-que-e-next-js/',
+            keywords: ['next', 'frontend', 'backend'],
+        },
+        {
+            title: 'Introduction to GraphQL',
+            type: 'Artigo',
+            url: 'https://graphql.org/learn/',
+            keywords: ['GraphQL', 'Introduction', 'iniciante'],
+        },
+    ],
 }
