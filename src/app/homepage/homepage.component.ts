5fa1779272fbb5c74bcfa85831f4d11d3efb8955
Elaine Manoelle <elaine.manoelle@pmipe.org.br>
2020-10-31 16:48:51 -0300

    Adjustment on home component

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,56 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import getArrays from '../datasource/references';
-import { MatDialog } from '@angular/material/dialog';
-import { FormContribuirDialogComponent } from  '../form-contribuir-dialog/form-contribuir-dialog.component'
+import { Component, OnInit } from '@angular/core'
+import getArrays from '../datasource/references'
+import { MatDialog } from '@angular/material/dialog'
+import { FormContribuirDialogComponent } from '../form-contribuir-dialog/form-contribuir-dialog.component'
 
-export interface Rerencia {
-  title: string;
-  type: string;
-  keywords: Array<string>;
-  url: string;
+export interface Referencia {
+    title: string
+    type: string
+    keywords: Array<string>
+    url: string
 }
 
-const ELEMENT_DATA: Rerencia[] = getArrays['links'];
+const ELEMENT_DATA: Array<Referencia> = getArrays['links']
 
 @Component({
     selector: 'app-homepage',
     templateUrl: './homepage.component.html',
-    styleUrls: ['./homepage.component.scss']
+    styleUrls: ['./homepage.component.scss'],
 })
-
 export class HomepageComponent implements OnInit {
-  displayedColumns: string[] = ['title', 'type', 'keywords', 'url'];
-  dataSource = ELEMENT_DATA;
-  searchParams: string = '';
+    displayedColumns: string[] = ['title', 'type', 'keywords', 'url']
+    dataSource = ELEMENT_DATA
+    searchParams: string = ''
 
-  constructor(
-    public dialog: MatDialog
-  ) { }
+    constructor(public dialog: MatDialog) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
-  filterLinks() {
-    if (this.searchParams && this.searchParams.length > 2) {
-      this.dataSource = this.dataSource.filter(item => {
-        if (item.title.includes(this.searchParams)  || item.type.includes(this.searchParams) || item.keywords.includes(this.searchParams) || item.url.includes(this.searchParams)) {
-          return true;
+    filterLinks() {
+        if (this.searchParams && this.searchParams.length > 2) {
+            this.dataSource = this.dataSource.filter((item) => {
+                if (
+                    item.title.includes(this.searchParams) ||
+                    item.type.includes(this.searchParams) ||
+                    item.keywords.includes(this.searchParams) ||
+                    item.url.includes(this.searchParams)
+                ) {
+                    return true
+                }
+            })
+        } else {
+            this.dataSource = getArrays['links']
         }
-      });
-    } else {
-      this.dataSource = getArrays['links'];
     }
-    
-  }
 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(FormContribuirDialogComponent, {
-      width: '250px',
-    });
+    openDialog(): void {
+        const dialogRef = this.dialog.open(FormContribuirDialogComponent, {
+            width: '250px',
+        })
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
-
+        dialogRef.afterClosed().subscribe((result) => {
+            console.log('The dialog was closed')
+        })
+    }
 }
